ae7649c60171f2ad2faa02e0725e278feef81f51
sachin bhalekar <sachinbansibhalekar@gmail.com>
2018-04-07 00:47:28 -0700

    Added functionality to provide LinkedIN GitHUb Resume Transcripts and HackerRank information

---
+++ ProfileInfo.js
@@ -0,0 +1,12 @@
+var ProfileInfo = Object.freeze({
+
+
+  LinkedIN: 'https:\/\/linkedin.com\/in\/sachin-bhalekar-37a18958\/',
+  GitHub: 'https:\/\/github.com\/sachinbhalekar',
+  HackerRank: 'https:\/\/www.hackerrank.com\/sachinbhalekar',
+  Resume: 'https:\/\/s3.us-east-2.amazonaws.com\/profiledocs\/Sachin+Bhalekar.pdf',
+  Transcripts: 'https:\/\/s3.us-east-2.amazonaws.com\/profiledocs\/transcript.pdf'
+
+});
+
+module.exports = ProfileInfo;
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var ProfileInfo = require('./ProfileInfo');
 var app = express();
 var port = process.env.PORT || 1337;
 
@@ -18,7 +18,72 @@ app.listen(port, function () {
 app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello ' + userName + ', welcome to Sachin\'s Slack channel ! I\'ll be your guide to fetch Sachin\'s profile. \n What would you like to know about Sachin \n 1. LinkedIn Profile \n 2. GitHub Profile \n 3. Resume \n '
+    text : 'Hello ' + userName + ', welcome to Sachin\'s Slack channel ! I\'ll be your guide to fetch Sachin\'s profile. \n What would you like to know about Sachin \n 1. LinkedIn Profile \n 2. GitHub Profile \n 3. Resume \n 4. Transcripts \n HackerRank Profile '
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.post('/linkedIN', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text :  'To Access LinkedIn Profile, Please visit the URL \n' + ProfileInfo.LinkedIN
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.post('/GitHub', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text :  'To Access GitHub Profile, Please visit the URL \n' + ProfileInfo.GitHub
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.post('/HackerRank', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text :  'To Access HackerRank Profile, Please visit the URL \n' + ProfileInfo.HackerRank
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.post('/Transcripts', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text :  'To Access Transcripts, Please visit the URL \n' + ProfileInfo.Transcripts
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.post('/Resume', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text :  'To Access Resume, Please visit the URL \n' + ProfileInfo.Resume
   };
   // Loop otherwise..
   if (userName !== 'slackbot') {
